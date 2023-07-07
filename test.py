c20b8edcb4ee1f71e41245a32387d714f49dedfc
LostOxygen <jonathan.evertz@gmail.com>
2023-07-07 13:50:30 +0200

    reboot project (because angular is fucking shit)

--- test.py
+++
@@ -1,5 +0,0 @@
-"""test file for backend"""
-from backend import llm_chat_api
-
-llm_chat_api.start_api()
-llm_chat_api.handle_message(user="jonny", message="hello", timestamp="2021-01-01 00:00:00")
