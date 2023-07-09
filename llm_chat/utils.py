1aba5dfd424ff1743177756a668657b9e5541412
jonathan <jonathan.evertz@gmail.com>
2023-07-09 12:21:13 +0200

    added nord color palette

--- llm_chat/utils.py
+++ llm_chat/utils.py
@@ -1,10 +1,12 @@
 """utility library for colors and stuff"""
 from typing import Final, List
 
-COLOR_LIST: Final[List[str]] = ["royalblue",
-                                "indianred",
-                                "seagreen",
-                                "orchid",
-                                "darkmagenta",
-                                "darkyellow",
-                                "darkgray"]
+from textual.color import Color
+
+COLOR_LIST: Final[List[str]] = [Color.parse("#5e81ac"),
+                                Color.parse("#a3be8c"),
+                                Color.parse("#d08770"),
+                                Color.parse("#b48ead"),
+                                Color.parse("#ebcb8b"),
+                                Color.parse("#bf616a"),
+                                Color.parse("#8fbcbb"),]
